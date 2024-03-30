a97a2a191b81a905e4b6ed4a993d6ea2ab556340
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2024-03-30 20:57:04 +0500

    initial Commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimary = Color(0xFF0D0D0D);
+const Color kSecondary = Color(0xFF191919);
+const Color kViolet = Color(0xFF8759F7);
+const Color kRed = Color(0xFFFF197B);
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kBlack = Color(0xFF0D0D0D);
+const Color kText = Color(0xFFA2A2A2);
+const Color kSubText = Color(0xFF191919);
+const Color kSubtitle = Color(0xFFB8B8B8);
+const Color kBorderColor = Color(0xFF322E2D);
+const Color kUnselectedItemColor = Color(0xFFB8B8B8);
+const Color kSelectedItemColor = Color(0xFFFFFFFF);
+const Color kNavigationContainerColor = Color(0xFF000B1D);
---
+++ lib/utils/apis.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+export '../appview.dart';
+export '../services/firebase_services.dart';
+export '../constants/colors.dart';
